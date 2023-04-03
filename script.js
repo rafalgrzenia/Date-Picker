e93615879b9e9165e195098250d1b90895a83cd0
Rafal Grzenia <rafalgrzenia96@gmail.com>
2023-04-03 12:37:12 +0200

    Add event listeners for dateButtons

--- script.js
+++ script.js
@@ -13,6 +13,18 @@ datePickerButton.addEventListener("click", () => {
   dateBoard.classList.toggle("show");
 });
 
+dateButtons.forEach((button) =>
+  button.addEventListener("click", (e) => {
+    const BoardDate = fullDate.innerText;
+    const day = e.target.innerText;
+    const month = format(new Date(BoardDate), "M");
+    const year = format(new Date(BoardDate), "Y");
+    const date = `${year},${month},${day}`;
+    setDate(date);
+    dateBoard.classList.toggle("show");
+  })
+);
+
 // Functions
 
 function setDate(date) {
