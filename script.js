e0481c5cba694fc48041ae9a48f62cccea3d84c3
Rafal Grzenia <rafalgrzenia96@gmail.com>
2023-04-04 11:05:22 +0200

    Add setCurrentMonth function and refactor setDate

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-import { format } from "date-fns";
+import { format, getUnixTime, fromUnixTime } from "date-fns";
 
 // Elements
 
 const datePickerButton = document.querySelector(".date-picker-button");
 const dateBoard = document.querySelector(".date-picker");
 const dateButtons = document.querySelectorAll(".date");
-const fullDate = document.querySelector(".current-month");
+const currentMonth = document.querySelector(".current-month");
+const previousMonthButton = document.querySelector(".prev-month-button");
+const nextMonthArrowButton = document.querySelector(".next-month-button");
 
 // Events
 
@@ -13,20 +15,29 @@ datePickerButton.addEventListener("click", () => {
   dateBoard.classList.toggle("show");
 });
 
-dateButtons.forEach((button) =>
-  button.addEventListener("click", (e) => {
-    const BoardDate = fullDate.innerText;
-    const day = e.target.innerText;
-    const month = format(new Date(BoardDate), "M");
-    const year = format(new Date(BoardDate), "Y");
-    const date = `${year},${month},${day}`;
-    setDate(date);
-    dateBoard.classList.toggle("show");
-  })
-);
+// dateButtons.forEach((button) =>
+//   button.addEventListener("click", (e) => {
+//     const BoardDate = fullDate.innerText;
+//     const day = e.target.innerText;
+//     const month = format(new Date(BoardDate), "M");
+//     const year = format(new Date(BoardDate), "Y");
+//     const date = `${year},${month},${day}`;
+//     setDate(date);
+//     dateBoard.classList.toggle("show");
+//   })
+// );
 
 // Functions
 
 function setDate(date) {
-  datePickerButton.innerText = format(new Date(date), "PPP");
+  datePickerButton.innerText = format(date, "MMMM do, yyyy");
+  datePickerButton.dataset.selectedDate = getUnixTime(date);
 }
+
+function setCurrentMonth(date) {
+  const selectedDate = fromUnixTime(datePickerButton.dataset.selectedDate);
+  currentMonth.innerText = format(selectedDate, "MMMM - yyyy");
+}
+
+setDate(new Date());
+setCurrentMonth(new Date());
