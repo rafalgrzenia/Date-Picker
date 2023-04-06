fb0d83ddd310bda6598c5ec5d7cef15dc88924cb
Rafal Grzenia <rafalgrzenia96@gmail.com>
2023-04-06 12:03:18 +0200

    Add setupDates function and refactor code

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // Libraries
 
-import { format, subMonths, addMonths, setDate, lightFormat } from "date-fns";
+import {
+  format,
+  subMonths,
+  addMonths,
+  setDate,
+  lightFormat,
+  startOfWeek,
+  startOfMonth,
+  endOfWeek,
+  endOfMonth,
+  eachDayOfInterval,
+} from "date-fns";
 
 // Elements
 
@@ -14,13 +25,30 @@ const previousMonthDays = document.querySelectorAll(
 );
 const currentMonth = document.querySelector(".current-month");
 const previousMonthButton = document.querySelector(".prev-month-button");
-const nextMonthArrowButton = document.querySelector(".next-month-button");
+const nextMonthButton = document.querySelector(".next-month-button");
+const datePickerGrid = document.querySelector(".date-picker-grid-dates");
+
 let currentDate = new Date();
 
 // Functions
 
+function setupDates() {
+  const firstWeekStart = startOfWeek(startOfMonth(currentDate));
+  const lastWeekEnd = endOfWeek(endOfMonth(currentDate));
+  const dates = eachDayOfInterval({ start: firstWeekStart, end: lastWeekEnd });
+  datePickerGrid.innerHTML = "";
+
+  dates.forEach((date) => {
+    const dateElement = document.createElement("button");
+    dateElement.classList.add("date");
+    dateElement.innerText = date.getDate();
+    datePickerGrid.append(dateElement);
+  });
+}
+
 function setCurrentDate(date) {
   datePickerButton.innerText = format(date, "MMMM do, yyyy");
+  setupDates();
 }
 
 function setMonth(selectedDate) {
@@ -48,6 +76,10 @@ function compareDates(firstDate, secondDate) {
   }
 }
 
+function classToggle(className) {
+  dateBoard.classList.toggle(className);
+}
+
 // Function Calls
 
 selectCurrentDay(currentDate);
@@ -70,14 +102,16 @@ previousMonthButton.addEventListener("click", () => {
   const currentMonthDate = lightFormat(new Date(), "MMMM-yyyy");
   const previousMonthDate = lightFormat(currentDate, "MMMM-yyyy");
   compareDates(currentMonthDate, previousMonthDate);
+  setupDates();
   setMonth(currentDate);
 });
 
-nextMonthArrowButton.addEventListener("click", () => {
+nextMonthButton.addEventListener("click", () => {
   currentDate = addMonths(currentDate, 1);
   const currentMonthDate = lightFormat(new Date(), "MMMM-yyyy");
   const nextMonthDate = lightFormat(currentDate, "MMMM-yyyy");
   compareDates(currentMonthDate, nextMonthDate);
+  setupDates();
   setMonth(currentDate);
 });
 
@@ -101,7 +135,3 @@ previousMonthDays.forEach((button) =>
     classToggle("show");
   })
 );
-
-function classToggle(className) {
-  dateBoard.classList.toggle(className);
-}
